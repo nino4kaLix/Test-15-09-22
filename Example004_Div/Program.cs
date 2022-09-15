b6a8eaac2e76797e8351c1e4a6e04231d5abb01f
Nina <nino4ka.lix@gmail.com>
2022-09-15 15:38:33 +0300

    add Example 003 Sum

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,3 +1,3 @@
-﻿double numberA = 12;
-double numberB = 5;
+﻿double numberA = new Random().Next(1,10); //0 1 2 ...9
+double numberB = new Random().Next(1,10); //0 1 2 ...9
 Console.WriteLine(numberA / numberB);
